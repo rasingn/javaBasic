91957278742f6a69050ff1930375d5ee336b285c
Younis <younis.alabri@omantel.om>
2025-07-31 13:40:30 +0400

    Libarary added

--- Book.java
+++ Book.java
@@ -1,7 +1,14 @@
 public class Book {
 
-    String Auth;
-    String Title;
+    private String Auth;
+    private String Title;
+
+
+    Book(String Auth, String Title){
+       this.Title=Title;
+       this.Auth=Auth;
+
+    }
 
     void print(){
         System.out.println(Title+" - "+Auth);
---
+++ Library.java
@@ -0,0 +1,18 @@
+public class Library {
+
+    public static void main(String[] args) {
+
+        Book book1 = new Book("Younis","java");       
+        book1.print();
+
+        Book book2 = new Book("Zainabu","Basic");
+        book2.print();
+
+        Book[]books={book1,book2};
+        
+      
+        
+        
+    }
+
+}
