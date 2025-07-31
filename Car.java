64149f594a124d3a4cf5145dbfab207012bde264
Younis <younis.alabri@omantel.om>
2025-07-31 13:03:07 +0400

    java basic

---
+++ Car.java
@@ -0,0 +1,24 @@
+public class Car {
+
+    static String color = "red"; 
+    static char model = 'a';
+    static int v= 1;
+    static String[]maintenance={"A","B","c"};
+
+    static void drive(String color){
+        // color="yellow";
+        color ="pink";
+        System.out.println("i have car with color "+ color+", model = "+model+" and v ="+v+", main ="+maintenance[2]);
+             
+    }
+
+    public static void main(String[] args) {
+        color="blue";
+        drive("black");
+        
+    }
+
+
+
+    
+}
