c483d0f58c8340ed06664d097dac00d38db17d39
Younis <younis.alabri@omantel.om>
2025-08-03 11:44:26 +0400

    for and while loop

--- Library.java
+++ Library.java
@@ -5,18 +5,27 @@ public class Library {
     public static void main(String[] args) {
 
       Book book1= new Book("Java","Younis");
-      book1.print();
-
-      book1.XX();
-
       Book book2 = new Book("Basic","Areej");
-      book2.print();
-
      
 
-  
 
-      
+     Book [] books={book1,book2};
+    // for (int i =0;i<books.length;i++){
+    //     books[i].print();
+    // }
+    
+    int i=0;
+    boolean flag=true;
+    while(i< books.length){
+        books[i].print();
+        i++;
+    }
+
+    
+
+
+     
+     
       
         
         
