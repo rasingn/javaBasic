d7a995f8ffddb5cc531d9af3dc85b37ca8801daa
Younis <younis.alabri@omantel.om>
2025-08-04 11:14:26 +0400

    termial Appl

--- Library.java
+++ Library.java
@@ -1,34 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Library {
 
-    
-   
     public static void main(String[] args) {
+        // Book boo1=;
 
-      Book book1= new Book("Java","Younis");
-      Book book2 = new Book("Basic","Areej");
-     
+        // Book [] books={};
+        List <Book>books=new ArrayList<Book>();
+        Scanner sacn = new Scanner(System.in);
+        int input = 0;
 
-
-     Book [] books={book1,book2};
-    // for (int i =0;i<books.length;i++){
-    //     books[i].print();
-    // }
-    
-    int i=0;
-    boolean flag=true;
-    while(i< books.length){
-        books[i].print();
-        i++;
-    }
-
-    
+        while (input != 4) {
+            System.out.println("Menu:\n 1- Add Book \n 2-Remove Book \n 3-Print All Books \n 4-Exit");
+            input = sacn.nextInt();
+            System.out.println("our input = " + input);
 
 
-     
-     
-      
-        
-        
+            switch (input) {
+                case 1:
+                    System.out.println("adding book");
+                    System.out.println("Enter Book title:");
+                    String t=sacn.next();
+                    
+                    System.out.println("Enter Book Auther:");
+                    String a=sacn.next();
+
+                    Book boo=new Book(t, a);
+                    books.add(boo);
+                    break;
+                case 2:
+                    System.out.println("removing");
+                    for(int i =0;i<books.size();i++){
+                        System.out.print(i+" - ");
+                        books.get(i).print();
+                    }
+                    System.out.println("Enter Book Number to remove");
+                    int ch=sacn.nextInt();
+                    if(ch>books.size()){
+                        System.out.println("Number is not Valid");
+                        break;
+                        
+                    }
+                    books.remove(ch);
+                case 3:
+                    System.out.println("print:");
+                    for(int i =0;i<books.size();i++){
+                        books.get(i).print();
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exit");
+                    break;
+                default:
+                 System.out.println("Numbers not included");
+
+            }
+
+            System.out.println("********************************");
+            System.out.println("********************************");
+        }
+
     }
 
 }
