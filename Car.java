f21f53f5ad7fe02b99a07dd9184e664962240274
khoula200 <khoulaalrawahi@gmail.com>
2025-11-25 10:30:40 +0400

    fixed the brackets and make the actions static

--- Car.java
+++ Car.java
@@ -3,16 +3,18 @@ public class Car {
     static String brand = "Toyota";
     static String color = "White";
   
-void start(){
-      System.out.println(brand + " car is driving")
+static void start(){
+      System.out.println(brand + " car is driving");
     }  
 
-void stop(){
-      System.out.println(brand + " car has stopped")
+static void stop(){
+      System.out.println(brand + " car has stopped");
     }
-  
-}
 
 public static void main(String[] args) {
+
+  stop();
  
+}
+
 }
